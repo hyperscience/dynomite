fd1d132a9639b18314ec08acb215c88ba9b70fb0
Vitali Baruh <vitali@hyperscience.com>
2020-02-28 17:35:28 +0200

    Make possible to persist redis state on docker host
    If redis is configured to save its states in RDB format it will
    execute one final save on SIGTERM signal.
    
    The change:
    * adds configuration for redis
    * ensures SIGTERM signal reaches both redis and dynomite
    * adds tini as a good container practice

--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+set -x
 
-#Start redis server on 22122
-redis-server --port 22122 &
+# the idea for this script is taken from here
+# https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
 
-src/dynomite --conf-file=conf/redis_single.yml -v5
+redis_pid=0
+dynomite_pid=0
+
+sig_term_handler() {
+    if [ "$redis_pid" != "0" ]; then
+        kill -SIGTERM "$redis_pid"
+        wait "$redis_pid"
+    fi
+    if [ "$dynomite_pid" != "0" ]; then
+        kill -SIGTERM "$dynomite_pid"
+        wait "$dynomite_pid"
+    fi
+}
+
+# killing the fail -f /dev/null first
+# then calling our handler
+trap 'kill ${!}; sig_term_handler' SIGTERM
+
+redis-server /etc/redis/redis.conf &
+redis_pid="$!"
+
+src/dynomite --conf-file=conf/redis_single.yml -v5 &
+dynomite_pid="$!"
+
+tail -f /dev/null &
+wait ${!}
